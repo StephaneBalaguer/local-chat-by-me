a70fc09ed469345c0fd62a7f29049ee41d7fb035
Stéphane Balaguer <st.balaguer@gmail.com>
2019-06-04 15:39:23 +0200

    + Separation des fichier js css dans le html. + Les messages ne peuvent plus etre interpretés par le navigateur + Commentarisation du code ( Oui "Commentarisation" existe) + Les liens sont desormais bien clickables * Mise a jour du README

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
 var express = require('express');
-var ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+var ent = require('ent'); /* Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)*/
 var app = express();
-
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var ipttp = require("ip");
 
 app.use(express.static('public'));
 
-//rendre le dossier "public" accessible 
+/*rendre le dossier "public" accessible */
 app.use(express.static('public'));
-// Chargement de la page index.html
+
+/* Chargement de la page index.html*/
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function (socket, pseudo) {
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    /* Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes*/
     socket.on('nouveau_client', function (pseudo) {
         socket.pseudo = pseudo;
         socket.broadcast.emit('nouveau_client', pseudo);
     });
 
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    /* Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes*/
     socket.on('message', function (message) {
+
+        var tab = message.split("\0");
+        message = ent.encode(tab[0]) + "\0" + tab[1];
         socket.broadcast.emit('message', { pseudo: socket.pseudo, message: message });
+        socket.emit('message', { pseudo: socket.pseudo, message: message });
     });
 
     socket.on('code', function (message) {
         message = ent.encode(message);
-        message = "<div id=\"code\">" + message + "</div>"
-        socket.broadcast.emit('message', { pseudo: socket.pseudo, message: message });
-        socket.emit('recepcode', { pseudo: socket.pseudo, message: message });
+        socket.broadcast.emit('code', { pseudo: socket.pseudo, message: message });
+        socket.emit('code', { pseudo: socket.pseudo, message: message });
     });
 });
 
-
-
-var ipttp = require("ip");
 console.log("==========================================");
 console.log("===================CHAT===================");
 console.log("==========================================");
---
+++ public/scripts/scriptclient.js
@@ -0,0 +1,160 @@
+var sonactif = true;
+var popip = window.location.href;
+window.addEventListener("onkeypress", checklatouche, false);
+
+/*Lors de l'appuie de la touche ENTREE : Envoie du message */
+function checklatouche(e) {
+   
+    if (e.keyCode == 13) {
+        envoiecouleur();
+        return false;
+    }
+}
+
+
+/*Connexion à socket.io*/
+var addrserv = popip.substr(0, popip.length - 1);
+var socket = io.connect(addrserv);
+
+
+/* On demande le pseudo, on l'envoie au serveur et on l'affiche dans le titre*/
+var pseudo = prompt('Quel est votre pseudo ?');
+socket.emit('nouveau_client', pseudo);
+document.title = pseudo + ' - ' + document.title;
+
+
+/** EVENEMENT  message
+ * ENTREE OBJET data  : data.message | data.pseudo
+ * ACTION : Affichage du message :  separation des infos dans le message ( message \0 couleur ) ajout des balises html et affichage du message
+ **/
+socket.on('message', function (data) {
+    tab = data.message.split("\0");
+    var messageaafficher = "<span style=\"color:" + tab[1] + "\"> " + lien(tab[0]) + " </span>";
+    insereMessage(data.pseudo, messageaafficher);
+    if (sonactif == true) {
+        document.getElementById("notif").play();
+    }
+})
+
+
+/** EVENEMENT  code
+ * ENTREE OBJET data  : data.message | data.pseudo
+ * ACTION : Affichage du message :  se ajout des balises html et affichage du message
+ **/
+socket.on('code', function (data) {
+    var message = "<div id=\"code\">" + data.message + "</div>"
+    insereMessage(data.pseudo, message);
+})
+
+
+/** EVENEMENT  nouveau_client
+ * ENTREE STRING pseudo
+ * ACTION : Affichage du message :  "--STRING pseudo-- a rejoint...."
+ **/
+socket.on('nouveau_client', function (pseudo) {
+    $('#zone_chat').prepend('<p><em>' + pseudo + ' a rejoint le Chat !</em></p>');
+    if (sonactif == true) {
+        document.getElementById('online').play();
+    }
+})
+
+
+/** FONCTION  insereMessage
+ * ENTREE : STRING pseudo , STRING message 
+ * SORTIE : 
+ * 
+ * ACTION :
+ * Ajoute dans la zone de chat:
+ * EN GRAS --STRING pseudo--
+ *         --STRING message-- 
+ **/
+function insereMessage(pseudo, message) {
+    $('#zone_chat').prepend('<p><strong>' + pseudo + '</strong> ' + message + '</p>');
+}
+
+
+/** FONCTION  ajoutbalisecode
+ * ENTREE :  
+ * SORTIE : false
+ * 
+ * ACTION :
+ * envoie le message avec l'information que c'est du code au serveur
+ **/
+function ajoutbalisecode() {
+    if ($('#message').val() != "") {
+        var message = $('#message').val();
+        socket.emit('code', message);
+
+        $('#message').val('').focus(); // Vide la zone de Chat et remet le focus dessus
+        return false; // Permet de bloquer l'envoi "classique" du formulaire
+    }
+}
+
+
+/** FONCTION  envoiecouleur
+ * ENTREE :  
+ * SORTIE : false
+ * 
+ * ACTION :
+ * envoie le message avec la couleur au serveur pour le broadcast a tous
+ * separateur utilise : \0
+ **/
+function envoiecouleur() {
+    if ($('#message').val() != "") {
+        var message = $('#message').val();
+        message = message + "\0" + document.getElementById("colorpicket").value;
+        socket.emit('message', message);
+        $('#message').val('').focus(); // Vide la zone de Chat et remet le focus dessus
+        return false; // Permet de bloquer l'envoi "classique" du formulaire
+    }
+}
+
+
+/**
+ * FONCTION lien
+ * ENTREE : STRING message 
+ * SORTIE : STRING out
+ * 
+ * ACTION :
+ * decode le message 
+ * recherche les liens http / https
+ * rends les lien clickable 
+ * encode les caracteres de nouveau
+ **/
+function lien(message) {
+    message = decode(message);
+    var out = "";
+    var too = message.split(' ');
+    for (i = 0; i < too.length; i++) {
+
+        var n = too[i].search("http://");
+        var m = too[i].search("https://");
+        if (n == 0 || m == 0) {
+            too[i] = "<a href=\"" + encode(too[i]) + "\" target=\"_blank\"\>" + encode(too[i]) + "</a>";
+            out += too[i] + " ";
+        } else {
+            out += encode(too[i]) + " ";
+        }
+    }
+    return out;
+}
+
+
+/**
+ * FONCTION changerson
+ * ENTREE : 
+ * SORTIE :  
+ * 
+ * ACTION : 
+ * active ou desactive le son 
+ **/
+function changerson() {
+    sonactif = document.getElementById("checkboxetatson").checked;
+    if (sonactif == true) {
+        document.getElementById('imagehp').src = 'images/hp-on.png';
+    }
+    else {
+        document.getElementById('imagehp').src = 'images/hp-off.png';
+    }
+
+}
